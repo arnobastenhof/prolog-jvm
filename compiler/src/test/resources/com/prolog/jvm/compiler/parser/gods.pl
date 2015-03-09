53c677febf894d9ae85fbeeed8d62055450ed6eb
Arno Bastenhof <arnobastenhof@gmail.com>
2015-03-09 12:46:28 +0100

    Expanded the grammar description and added unit tests.

---
+++ compiler/src/test/resources/com/prolog/jvm/compiler/parser/gods.pl
@@ -0,0 +1,22 @@
+/*
+ * This example is adapted from Kowalski (1979), Algorithm = Logic + Control.
+ */
+
+% Facts.
+father(zeus, ares).
+mother(hera, ares).
+father(ares, harmonia).
+mother(semele, dionisius).
+father(zeus, dionisius).
+
+% Chain rules.
+parent(X,Y) :- mother(X,Y).
+parent(X,Y) :- father(X,Y).
+ancestor(X,Y) :- parent(X,Y).
+
+% Rules with more than one goal.
+grandparent(X,Y) :- parent(X,Z), parent(Z,Y).
+ancestor(X,Y) :- ancestor(X,Z), ancestor(Z,Y).
+
+% Query.
+?- ancestor(zeus, X).
---
+++ compiler/src/test/resources/com/prolog/jvm/compiler/parser/query.pl
@@ -0,0 +1,6 @@
+/*
+ * This example is adapted from Kowalski (1979), Algorithm = Logic + Control.
+ */
+
+% Query.
+?- ancestor(zeus, X).
