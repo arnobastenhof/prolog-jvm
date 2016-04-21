ec7b416284176bfee8df4fad2143efcb780996b4
Arno Bastenhof <arnobastenhof@gmail.com>
2016-04-21 11:15:56 +0200

    Tests: Added unit tests for the instructions utility class.

---
+++ src/test/java/com/prolog/jvm/zip/util/InstructionsTest.java
@@ -0,0 +1,20 @@
+package com.prolog.jvm.zip.util;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public final class InstructionsTest {
+
+	@Test
+	public void getMode() {
+		assertEquals(0xC0, Instructions.getMode(0xC0));
+		assertEquals(0x0, Instructions.getMode(0x3F));
+	}
+
+	@Test
+	public void getOpcode() {
+		assertEquals(0x0, Instructions.getOpcode(0xC0));
+		assertEquals(0x3F, Instructions.getOpcode(0x3F));
+	}
+}
